f3895a7f6edad798bc2ea120852f42de7d58764e
Bobur Khatamov <97375635+BoburXO@users.noreply.github.com>
2024-01-20 12:47:47 +0500

    Update HomeHeader.tsx

--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -4,13 +4,13 @@ const HomeHeader = () => {
   return (
     <>
       <div className="container">
-       <h1 className="home_header_title">Welcome To MacBro Family!</h1>
+       <h1 className="home_header_title">Welcome To BroStore Family!</h1>
        <br />
        <br />
         <img
         className="home_header_img"
-          src="https://cdn.macbro.uz/macbro/8edef17b-e87b-4ac4-94e6-4bb822b94817"
-          alt=""
+          src="https://brostore.uz/cdn/shop/files/Banner-ru_3840x2160_crop_center.jpg?v=1705153153"
+          alt="Bro Store"
         />
       </div>
     </>
